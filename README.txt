3bd411b772bff6d8dca0fd3b3fcbd07576148d3d
dwmedeiros94 <davidmedeiros94@hotmail.com>
2022-06-25 14:39:17 -0300

    Done

---
+++ README.txt
@@ -0,0 +1,11 @@
+// Crie um objeto que receba ao menos três propriedades sobre você
+
+// Adicione uma nova propriedade sem alterar seu objeto inicial
+
+// Remova uma propriedade desse objeto
+
+//Mostre no console todas as propriedades desse objeto
+
+// Crie um array  chamado "cadastro" contendo ao menos 5 objetos.
+// Cada objeto deve receber as seguintes propriedades: nome,  idade,  telefone, amigos
+// Na propriedade amigos, adicione ao menos 4 itens
---
+++ script.js
@@ -0,0 +1,60 @@
+// EXERCÍCIO 1
+
+let data = {
+    Name: "David",
+    Age: 28,
+    Team: "Flamengo"
+}
+console.log(data)
+
+
+// EXERCÍCIO 2
+
+data.nationality = "Brasileira"
+console.log(data)
+
+
+// EXERCÍCIO 3
+
+delete data.nationality
+console.log(data)
+
+
+// EXERCÍCIO 4
+
+console.log(data)
+
+
+// EXERCÍCIO 5
+
+let information = [{
+    Nome: "Maria",
+    Idade: 25,
+    Telefone: +55219987654321,
+    Amigos: ["Leo", "Rômulo", "Júlia", "Mario"]
+},
+{
+    Nome: "Matheus",
+    Idade: 30,
+    Telefone: +55219987654322,
+    Amigos: ["Rafael", "Maicon", "Flavisson", "David"]
+},
+{
+    Nome: "Victor",
+    Idade: 33,
+    Telefone: +55219987654323,
+    Amigos: ["Thiago", "Marcelo", "Pedro", "Juliana"]
+},
+{
+    Nome: "Luis",
+    Idade: 25,
+    Telefone: +55219987654324,
+    Amigos: ["Fernanda", "Mariana", "Igor", "Rodrigo"]
+},
+{
+    Nome: "Ricardo",
+    Idade: 20,
+    Telefone: +55219987654325,
+    Amigos: ["Jonathan", "Fernando", "Felipe", "Eduarda"]
+}]
+console.log(information)
